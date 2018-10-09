82a0b3737bd1dbb96af8ad8f14e5a151e8d8c948
Philip <philipjunker@gmail.com>
2018-10-09 21:30:46 +0200

    .

--- src/WorkerThread.java
+++ src/WorkerThread.java
@@ -97,7 +97,7 @@ public class WorkerThread implements Runnable {
         for (int serverIdx = 0; serverIdx < serverConnections.length; serverIdx++) {
             SocketChannel serverChannel = serverConnections[serverIdx];
             logger.info(String.format("Worker %d sends set request to memcached server %d...", this.id, serverIdx));
-            if(!serverChannel.isConnected())
+            if(!serverChannel.isConnected() || !serverChannel.isOpen())
                 logger.info("ERROR: no connection to memcached");
             request.buffer.rewind();
             while (request.buffer.hasRemaining()) {
