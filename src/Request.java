c569ec5311c5ff79871da3c1b6d63085c8d3eaa8
Philip <philipjunker@gmail.com>
2018-10-15 16:21:01 +0200

    clean Request.java

--- src/Request.java
+++ src/Request.java
@@ -2,8 +2,8 @@ package ch.ethz.asltest;
 
 import java.util.*;
 
-import java.nio.channels.SocketChannel;
 import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
 
 
@@ -18,18 +18,24 @@ public class Request {
     static final int VALUE_SIZE_MAX = 4096;        // According to instructions
     private static final Logger logger = LogManager.getLogger("Request");
 
-    private Type type = Type.NOT_SET;
-    private SocketChannel requestorChannel;
-    ByteBuffer buffer;
-    String requestStr;
-    List<Integer> offsets;
+    private Type type = Type.NOT_SET;               // The type of this request
+    private SocketChannel requestorChannel;         // The socketchannel to the memtier client
+    ByteBuffer buffer;                              // The buffer which stores the memtier request
+    String requestStr;                              // For get requests the buffer is converted into a string for convenience
+    List<Integer> offsets;                          // A list of space-character-indizes. The last index is of '\r' character
 
+    /**
+     * Constructor
+     */
     public Request(SocketChannel channel) {
         this.requestorChannel = channel;
         this.buffer = ByteBuffer.allocate(HEADER_SIZE_MAX + VALUE_SIZE_MAX);   // using non-direct bytebuffer here
         offsets = new ArrayList<Integer>();
     }
 
+    /**
+     * Encodes the type of the request
+     */
     public enum Type {
         GET,
         MULTIGET,
@@ -38,10 +44,16 @@ public class Request {
         UNKNOWN;
     }
 
+    /**
+     * Accessor function for requestorChannel
+     */
     public SocketChannel getRequestorChannel() {
         return this.requestorChannel;
     }
 
+    /**
+     * Reset current object such that it can be reused
+     */
     public void reset(SocketChannel channel) {
         this.requestorChannel = channel;
         this.type = Type.NOT_SET;
@@ -50,6 +62,10 @@ public class Request {
         this.requestStr = "";
     }
 
+    /**
+     * Parses the type of the request stored in this.buffer.
+     * Result is returned and stored in this.type
+     */
     public Type getType() {
         if(this.type == Type.NOT_SET) {
             byte firstChar = this.buffer.get(0);
@@ -77,10 +93,17 @@ public class Request {
         return this.type;
     }
 
+    /**
+     * isComplete for this object
+     */
     public boolean isComplete() {
         return isComplete(this.buffer);
     }
 
+    /**
+     * Checks whether the request stored in buf is already complete.
+     * Both read and write mode are supported, end is identified by '\n' character
+     */
     public static boolean isComplete(ByteBuffer buf) {
         boolean res = false;
         logger.debug(String.format("Checking if buffer is completed: position: %d limit: %d capacity: %d", buf.position(), buf.limit(), buf.capacity()));
@@ -105,6 +128,10 @@ public class Request {
         return res;
     }
 
+    /**
+     * Given the number of memcached servers it splits the multiget request into several (up to numServers) requests.
+     * Returns only the keys per request which need to be added into a request in the following way: get <keys>\r\n
+     */
     public ByteBuffer[] splitGetsKeys(int numServers) {
         // requires parseGet() to be executed beforehand. This function is executed by getType() in NetworkerThread
         logger.debug(String.format("Splitting multiget keys for %d servers", numServers));
@@ -152,6 +179,7 @@ public class Request {
     /**
      * Converts buffer to String and parses it for space characers and endline. 
      * String is stored in this.requestStr and space positions are stored in this.offsets
+     * The last element of this.offsets is the position of \r
      * Returns the number of white spaces that are contained in the request
      */
     private int parseGet() {
@@ -169,24 +197,10 @@ public class Request {
         return offsets.size();
     }
 
-    public int numGets() {
-        int res = 0;
-        if (isComplete()) {
-            switch (this.type) {
-                case MULTIGET:
-                case GET:           // get can also be multiget according to protocol rules
-                    res = parseGet();
-                    break;
-                default:
-                    break;
-            }
-        }
-        else {
-            res = -1;
-        }
-        return res;
-    }
-
+    /**
+     * Converts a bytebuffer into a string
+     * Does not affect pointers of bytebuffer
+     */
     public static String byteBufferToString(ByteBuffer buf) {
         final byte[] bytes = new byte[buf.remaining()];
      
@@ -195,30 +209,34 @@ public class Request {
         return new String(bytes);
     }
 
+    /**
+     * Converts a string into a bytebuffer
+     */
     public static ByteBuffer stringToByteBuffer(String str) {
         return StandardCharsets.US_ASCII.encode(str);
     }
 
+    /**
+     * Checks if GET response sent by memcached server is complete.
+     * Checks for "END\r\n" in the end of responseBuf
+     */
     public static boolean getResponseIsComplete(ByteBuffer responseBuf) {
         ByteBuffer buf = responseBuf.duplicate();
         int current_pos = buf.position();
+        boolean res = false;
         if(current_pos >= 5) {
             byte fifth = buf.get(current_pos-5);
             byte fourth = buf.get(current_pos-4);
             byte third = buf.get(current_pos-3);
             byte snd = buf.get(current_pos-2);
             byte fst = buf.get(current_pos-1);
-            boolean res =  (fifth == 'E' && 
+            res =  (fifth == 'E' && 
                     fourth == 'N' && 
                     third == 'D' && 
                     snd == '\r' && 
                     fst == '\n'); 
-            logger.debug(String.format("Response ends with following characters:%c%c%c%c%c complete: %b", fifth, fourth, third, snd, fst, res));
-
-            return res;
-        } else {
-            return false;
         }
+        return res;
     }
 
 }
