1f3a6528a9788e2771465a60e4147534be700a07
Philip <philipjunker@gmail.com>
2018-10-15 12:47:08 +0200

    .

--- src/Request.java
+++ src/Request.java
@@ -85,24 +85,23 @@ public class Request {
     public static boolean isComplete(ByteBuffer buf) {
         boolean res = false;
         logger.debug(String.format("Checking if buffer is completed: position: %d limit: %d capacity: %d", buf.position(), buf.limit(), buf.capacity()));
+        byte lastChar = 'a';
         if(buf.position() > 0) {
-            byte lastChar = buf.get(buf.position()-1);
-            res = lastChar == '\n';
-            if(res) {
-                logger.debug("last character of buffer is newline, request is complete");
-            }
-            else {
-                logger.debug(String.format("last character of buffer is not newline, request not finished yet: %c", lastChar));
-            }
-            return res;
+            logger.debug("Checking lastchar behind buf.position()");
+            lastChar = buf.get(buf.position()-1);
         }
         else {
-            if (buf.limit() < buf.capacity()) {
-                ByteBuffer b = buf.duplicate();
-                b.flip();
-                logger.debug(String.format("buffer flipped for complete check: position: %d limit: %d capacity: %d", b.position(), b.limit(), b.capacity()));
-                if(b.position() > 0) res = isComplete(b);
+            if(buf.limit() < buf.capacity()) {
+                logger.debug("Checking lastchar behind buf.limit()");
+                lastChar = buf.get(buf.limit()-1);
             }
+        }
+        res = lastChar == '\n';
+        if(res) {
+            logger.debug("last character of buffer is newline, request is complete");
+        }
+        else {
+            logger.debug(String.format("last character of buffer is not newline, request not finished yet: %c", lastChar));
         }
         return res;
     }
