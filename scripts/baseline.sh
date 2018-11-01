39cee5a385ec93f14a438ff23564d3a808d90791
Philip <philipjunker@gmail.com>
2018-11-01 16:04:27 +0100

    fix baseline script

--- scripts/baseline.sh
+++ scripts/baseline.sh
@@ -14,7 +14,7 @@ ssh -o StrictHostKeyChecking=no junkerp@${server2} "screen -L -dm -S server2 mem
 ssh -o StrictHostKeyChecking=no junkerp@${server3} "screen -L -dm -S server3 memcached -p 11212 -vv &> server3.log"
 
 # start middleware1
-ssh -o StrictHostKeyChecking=no junkerp@${MW1} "screen -L -dm -S middleware1 cd asl; java -jar dist/middleware-junkerp.jar  -l ${MW1} -p 1234 -t 2 -s true -m ${server1}:11212 ${server2}:11212 ${server3}:11212 &> middleware1.log"
+ssh -o StrictHostKeyChecking=no junkerp@${MW1} "cd asl; screen -L -dm -S middleware1 java -jar dist/middleware-junkerp.jar  -l ${MW1} -p 1234 -t 2 -s true -m ${server1}:11212 ${server2}:11212 ${server3}:11212 &> logs/middleware1.log"
 # initialize memcached servers with all keys
 ssh -o StrictHostKeyChecking=no junkerp@${client1} 'memtier_benchmark --server=${MW1} --port=11212 --clients=1 --requests=10000 --protocol=memcache_text --run-count=1 --threads=1 --debug --key-maximum=10000 --ratio=1:0 --data-size=4096 --key-pattern=S:S &> memtier1.log'
 #run the command
@@ -32,7 +32,8 @@ ssh -o StrictHostKeyChecking=no junkerp@${client1} 'memtier_benchmark --server=$
 #
 # 2.2b) Write only, 2 memtier clients with 1 thread each, 2 memcached server
 # virtual clients per memtier client 1..32
-# ssh -v -o StrictHostKeyChecking=no junkerp@10.0.0.5 'memtier_benchmark --server=10.0.0.10 --port=11212 --clients=1 --requests=10000 --protocol=memcache_text --run-count=1 --threads=1 --debug --key-maximum=10000 --ratio=1:0 --data-size=4096 --key-pattern=S:S &> memtier1.log'
+# ssh -v -o StrictHostKeyChecking=no junkerp@10.0.0.10 "cd asl; screen -L -dm -S middleware1 java -jar dist/middleware-junkerp.jar  -l 10.0.0.10 -p 1234 -t 2 -s true -m 10.0.0.8:11212 10.0.0.7:11212 10.0.0.11:11212 &> logs/middleware1.log"
+
 # Cleanup
 screen -X -S server1 quit
 ssh -v -o StrictHostKeyChecking=no junkerp@${server2} "screen -X -S server2 quit"
