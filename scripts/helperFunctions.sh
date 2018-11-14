0a6f4d01d9408d4fb9aad39b2d984b2a5bf1c712
Philip <philipjunker@gmail.com>
2018-11-14 17:59:16 +0100

    .

--- scripts/helperFunctions.sh
+++ scripts/helperFunctions.sh
@@ -503,7 +503,7 @@ stopMiddleware() {
     ip=$1
     designator=$2
     log "Stopping ${designator} (ip=${ip})"
-    screencmd="screen -X -S ${designator} quit; ls; cnt=0; while [[ ! -f ~/asl/logs/done.info && ${cnt} -lt 50 ]]; do cnt=$((cnt + 1)); sleep 0.1; done; rm ~/asl/logs/done.info;"
+    screencmd="screen -X -S ${designator} quit; ls; cnt=0; while [[ ! -f ~/asl/logs/done.info && ${cnt} -lt 50 ]]; do cnt=$((cnt + 1)); sleep 0.1; done; echo ${cnt}; rm ~/asl/logs/done.info;"
     ssh -o StrictHostKeyChecking=no junkerp@${ip} "${screencmd}"
 }
 
