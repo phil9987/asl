663f3bc914b5972f30f53c87a94f3b55d1e2eaf6
phil9987 <junkerp@student.ethz.ch>
2018-11-18 14:48:42 +0100

    remove unnecessary local variables

--- scripts/helperFunctions.sh
+++ scripts/helperFunctions.sh
@@ -5,9 +5,8 @@ source variables.sh
 killScreen() {
     #args
     # $1: ip
-    ip=$1
-    log "killing all screen sessions of ${ip}"
-    ssh -o StrictHostKeyChecking=no junkerp@${ip} "killall screen"
+    log "killing all screen sessions of $1"
+    ssh -o StrictHostKeyChecking=no junkerp@$1 "killall screen"
 }
 
 stopAllMW1() {
@@ -45,10 +44,8 @@ startDstat() {
     #args
     # $1: ip
     # $2: designator
-    ip=$1
-    designator=$2
-    designatordstat="${designator}${DSTATDESIGNATOR}"
-    ssh -o StrictHostKeyChecking=no junkerp@${ip} "screen -dm -S ${designatordstat} dstat -clmn --noheaders --output ${DSTATFILE}"
+    designatordstat="$2${DSTATDESIGNATOR}"
+    ssh -o StrictHostKeyChecking=no junkerp@$1 "screen -dm -S ${designatordstat} dstat -clmn --noheaders --output ${DSTATFILE}"
 }
 
 startDstatMW1() {
@@ -90,35 +87,29 @@ startPing() {
     # $2: ip to ping
     # $3: designator origin
     # $4: designator destination
-    ip=$1
-    iptoping=$2
     designator=$3$4
     designatorping="${designator}${PINGDESIGNATOR}"
     log "Starting ping with screen session ${designatorping}, writing to file ${designator}${PINGFILE}"
-    ssh -o StrictHostKeyChecking=no junkerp@${ip} "screen -dm -S ${designatorping} bash -c 'ping -nD ${iptoping} &> ${PINGFILE}${designator}'"
+    ssh -o StrictHostKeyChecking=no junkerp@$1 "screen -dm -S ${designatorping} bash -c 'ping -nD $2 &> ${PINGFILE}${designator}'"
 }
 
 # moves experiment.log to destination path
 moveExperimentLog() {
     #args
     # $1: destination path
-    destpath=$1
-    echo "moving experiment.log to ${destpath}"
-    mv experiment.log ${destpath}/experiment.log
+    echo "moving experiment.log to $1"
+    mv experiment.log $1/experiment.log
 }
 
 # creates a directory with name arg1
 createDirectory() {
     #args
     # $1: the name of the new directory
-    dir=$1
-    mkdir -p ${dir}
+    mkdir -p $1
 }
 
 createRemoteDirectory() {
-    ip=$1
-    dir=$2
-    ssh -o StrictHostKeyChecking=no junkerp@${ip} "mkdir -p ${dir}"
+    ssh -o StrictHostKeyChecking=no junkerp@$1 "mkdir -p $2"
 }
 
 # removes a file from a remote server
@@ -126,10 +117,8 @@ removeFile() {
     #args
     # $1: ip
     # $2: file
-    ip=$1
-    file=$2
-    echo "deleting file ${file} from ${ip}"
-    ssh -o StrictHostKeyChecking=no junkerp@${ip} "rm ${file}"
+    echo "deleting file $2 from $1"
+    ssh -o StrictHostKeyChecking=no junkerp@$1 "rm $2"
 }
 
 collectLogs() {
@@ -137,54 +126,46 @@ collectLogs() {
     # $1: path to copy logfiles to
     # $2: ip
     # $3: designator
-    path=$1
-    ip=$2
-    designator=$3
-    echo "Collecting logs from ${designator} ($ip, $path)"
-    dir="${path}/${designator}"
+    echo "Collecting logs from $3 ($2, $1)"
+    dir="$2/$3"
     createDirectory ${dir}
-    scp -o StrictHostKeyChecking=no junkerp@${ip}:~/asl/logs/* ${dir}
-    ssh -o StrictHostKeyChecking=no junkerp@${ip} "rm -r ~/asl/logs; mkdir -p ~/asl/logs"
+    scp -o StrictHostKeyChecking=no junkerp@$2:~/asl/logs/* ${dir}
+    ssh -o StrictHostKeyChecking=no junkerp@$2 "rm -r ~/asl/logs; mkdir -p ~/asl/logs"
 }
 
 # collects all relevant logs from MW1
 collectLogsFromMiddleware1() {
     #args
     # $1: path to copy the logfiles to
-    path=$1
-    collectLogs ${path} ${MW1IP} ${MW1DESIGNATOR}
+    collectLogs $1 ${MW1IP} ${MW1DESIGNATOR}
 }
 
 # collects all relevant logs from MW2
 collectLogsFromMiddleware2() {
     #args
     # $1: path to copy the logfiles to
-    path=$1
-    collectLogs ${path} ${MW2IP} ${MW2DESIGNATOR}
+    collectLogs $1 ${MW2IP} ${MW2DESIGNATOR}
 }
 
 # collects all relevant logs from SERVER1
 collectLogsFromServer1(){
     #args
     # $1: path to copy the logfiles to
-    path=$1
-    collectLogs ${path} ${SERVER1IP} ${SERVER1DESIGNATOR}
+    collectLogs $1 ${SERVER1IP} ${SERVER1DESIGNATOR}
 }
 
 # collects all relevant logs from SERVER2
 collectLogsFromServer2(){
     #args
     # $1: path to copy the logfiles to
-    path=$1
-    collectLogs ${path} ${SERVER2IP} ${SERVER2DESIGNATOR}
+    collectLogs $1 ${SERVER2IP} ${SERVER2DESIGNATOR}
 }
 
 # collects all relevant logs from SERVER3
 collectLogsFromServer3(){
     #args
     # $1: path to copy the logfiles to
-    path=$1
-    collectLogs ${path} ${SERVER3IP} ${SERVER3DESIGNATOR}
+    collectLogs $1 ${SERVER3IP} ${SERVER3DESIGNATOR}
 }
 
 # collects the logfiles from client1 (client from which script is executed)
@@ -192,9 +173,8 @@ collectLogsFromClient1() {
     # This client is the one running the commands, hence it is local
     #args
     # $1: path to copy the logfiles to
-    path=$1
-    echo "Collecting logs from ${CLIENT1DESIGNATOR} (local, ${path})"
-    dir="${path}/${CLIENT1DESIGNATOR}"
+    echo "Collecting logs from ${CLIENT1DESIGNATOR} (local, $1)"
+    dir="$1/${CLIENT1DESIGNATOR}"
     createDirectory ${dir}
     mv ~/asl/logs/* ${dir}
 }
@@ -203,16 +183,14 @@ collectLogsFromClient1() {
 collectLogsFromClient2() {
     #args
     # $1: path to copy the logfiles to
-    path=$1
-    collectLogs ${path} ${CLIENT2IP} ${CLIENT2DESIGNATOR}
+    collectLogs $1 ${CLIENT2IP} ${CLIENT2DESIGNATOR}
 }
 
 # collects all relevant logs from CLIENT3
 collectLogsFromClient3() {
     #args
     # $1: path to copy the logfiles to
-    path=$1
-    collectLogs ${path} ${CLIENT3IP} ${CLIENT3DESIGNATOR}
+    collectLogs $1 ${CLIENT3IP} ${CLIENT3DESIGNATOR}
 }
 
 # Starts all 3 servers
@@ -272,30 +250,22 @@ runMemtierClient() {
     # $6: numthreads e.g. 2
     # $7: ${FIRSTMEMTIER} if this is the first instance of memtier on this machine or ${SECONDMEMTIER} if this is the second instance
     # $8: client_IP (if not locally executed)
-    ip=$1
-    port=$2
-    numclients=$3
-    ratio=$4
-    designator=$5
-    numthreads=$6
-    instance=$7
-    logname=${designator}${instance}
-    log "ip=$ip port=$port numclients=$numclients ratio=$ratio designator=$designator numthreads=$numthreads instance=$instance"
-    basecmd="memtier_benchmark --server=${ip} --port=${port} --clients=${numclients} --test-time=${TESTTIME} --ratio=${ratio} --protocol=memcache_text --run-count=1 --threads=${numthreads} --key-maximum=10000 --data-size=4096 "
+    logname=$5$7
+    log "memtier parameters ip=$1 port=$2 numclients=$3 ratio=$4 designator=$5 numthreads=$6 instance=$7 logname=${logname}"
+    basecmd="memtier_benchmark --server=$1 --port=$2 --clients=$3 --test-time=${TESTTIME} --ratio=$4 --protocol=memcache_text --run-count=1 --threads=$6 --key-maximum=10000 --data-size=4096 "
     if [[ $# -eq 7 ]]; then
-        if [[ ${instance} == ${FIRSTMEMTIER} ]]; then
-            log "starting memtier ${designator} (local, ${instance}, blockingmode) connected to ${ip}:${port} with clients=${numclients} threads=${numthreads} and a ratio of ${ratio} writing logs to screenlog.0"
+        if [[ $7 == ${FIRSTMEMTIER} ]]; then
+            log "starting memtier $5 (local, $7, blockingmode)"
             cmd="${basecmd} --out-file=../logs/${logname}.log --json-out-file=../logs/${logname}.json"
             log "$cmd"
             $cmd
         else
-            log "starting memtier ${designator} (local, ${instance}) connected to ${ip}:${port} with clients=${numclients} threads=${numthreads} and a ratio of ${ratio} writing logs to screenlog.0"
+            log "starting memtier $5 (local, $7, nonblocking)"
             screen -dm -S ${logname} ${basecmd} --out-file=asl/logs/${logname}.log --json-out-file=asl/logs/${logname}.json
         fi
     elif [[ $# -eq 8 ]]; then
-        clientIP=$8
-        log "starting memtier ${designator} (remote, ${instance}) connected to ${ip}:${port} with clients=${numclients} threads=${numthreads} and a ratio of ${ratio} writing logs to screenlog.0"
-        ssh -o StrictHostKeyChecking=no junkerp@${clientIP} "screen -dm -S ${logname} ${basecmd} --out-file=asl/logs/${logname}.log --json-out-file=asl/logs/${logname}.json"
+        log "starting memtier ${designator} (remote, ${instance}, clientIP=$8)"
+        ssh -o StrictHostKeyChecking=no junkerp@$8 "screen -dm -S ${logname} ${basecmd} --out-file=asl/logs/${logname}.log --json-out-file=asl/logs/${logname}.json"
     else
         log "ERROR: invalid number of arguments (expected 7 for local and 8 for remote client execution): $#"
     fi
@@ -309,20 +279,16 @@ startMiddleware() {
     # $3: numservers
     # $4: numWorkerThreads
     # $5: sharded
-    ip=$1
-    designator=$2
-    numservers=$3
-    numthreads=$4
-    sharded=$5
-    log "Starting $designator with ${numservers} servers (ip=$ip)"
+    log "Starting $2 with $3 servers (ip=$1)"
+    basecmd="cd asl; screen -dm -S $2 java -jar dist/middleware-junkerp.jar  -l $1 -p ${MWPORT} -t $4 -s $5 -m ${SERVER1IP}:${MEMCACHEDPORT}"
     if [[ ${numservers} -eq 1 ]]; then
-        ssh -o StrictHostKeyChecking=no junkerp@${ip} "cd asl; screen -dm -S ${designator} java -jar dist/middleware-junkerp.jar  -l ${ip} -p ${MWPORT} -t ${numthreads} -s ${sharded} -m ${SERVER1IP}:${MEMCACHEDPORT}"
+        ssh -o StrictHostKeyChecking=no junkerp@$1 $basecmd
     elif [[ ${numservers} -eq 2 ]]; then
-        ssh -o StrictHostKeyChecking=no junkerp@${ip} "cd asl; screen -dm -S ${designator} java -jar dist/middleware-junkerp.jar  -l ${ip} -p ${MWPORT} -t ${numthreads} -s ${sharded} -m ${SERVER1IP}:${MEMCACHEDPORT} ${SERVER2IP}:${MEMCACHEDPORT}"
+        ssh -o StrictHostKeyChecking=no junkerp@$1 "$basecmd ${SERVER2IP}:${MEMCACHEDPORT}"
     elif [[ ${numservers} -eq 3 ]]; then
-        ssh -o StrictHostKeyChecking=no junkerp@${ip} "cd asl; screen -dm -S ${designator} java -jar dist/middleware-junkerp.jar  -l ${ip} -p ${MWPORT} -t ${numthreads} -s ${sharded} -m ${SERVER1IP}:${MEMCACHEDPORT} ${SERVER2IP}:${MEMCACHEDPORT} ${SERVER3IP}:${MEMCACHEDPORT}"
+        ssh -o StrictHostKeyChecking=no junkerp@$1 "$basecmd ${SERVER2IP}:${MEMCACHEDPORT} ${SERVER3IP}:${MEMCACHEDPORT}"
     else
-        log "ERROR: cannot start middleware. Invalid parameter for numservers: ${numservers}"
+        log "ERROR: cannot start middleware. Invalid parameter for numservers: $3"
     fi
     sleep 5
     log "Middleware started"
@@ -334,10 +300,7 @@ startMiddleware1() {
     # $1: numservers
     # $2: numWorkerThreads
     # $3: sharded
-    numservers=$1
-    numthreads=$2
-    sharded=$3
-    startMiddleware ${MW1IP} ${MW1DESIGNATOR} ${numservers} ${numthreads} ${sharded}
+    startMiddleware ${MW1IP} ${MW1DESIGNATOR} $1 $2 $3
 }
 
 startMiddleware2() {
@@ -345,20 +308,15 @@ startMiddleware2() {
     # $1: numservers
     # $2: numWorkerThreads
     # $3: sharded
-    numservers=$1
-    numthreads=$2
-    sharded=$3
-    startMiddleware ${MW2IP} ${MW2DESIGNATOR} ${numservers} ${numthreads} ${sharded}
+    startMiddleware ${MW2IP} ${MW2DESIGNATOR} $1 $2 $3
 }
 
 stopDstat() {
     #args
     # $1: ip
     # $2: designator
-    ip=$1
-    designator=$2
-    designatordstat="${designator}${DSTATDESIGNATOR}"
-    ssh -o StrictHostKeyChecking=no junkerp@${ip} "screen -X -S ${designatordstat} quit"
+    designatordstat="$2${DSTATDESIGNATOR}"
+    ssh -o StrictHostKeyChecking=no junkerp@$1 "screen -X -S ${designatordstat} quit"
 }
 
 stopDstatServer1() {
