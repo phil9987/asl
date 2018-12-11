cdf477bbd7710ee9d2740eecded5da292bb1ba3e
phil9987 <philipjunker@gmail.com>
2018-12-11 17:29:11 +0100

    prepare for sec5 full run

--- scripts/run_all_experiments.sh
+++ scripts/run_all_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Run this script from the first memtier client
 #Make sure variables.sh and helperFunctions.sh are in the same directory
-#./init.sh
+./init.sh
 #./repeat5.sh
 #./sec2_baseline.sh
 #./sec3_baselineMW.sh
